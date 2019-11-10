bf029880b77b9bd64d53ab458a0afd6a6413b437
xiaoju <xiaoju.zh@qq.com>
2019-11-11 06:19:56 +0800

    -

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,9 +3,6 @@ const app = getApp()
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
     bannerList: [
       {
         src: '../../images/banner1.jpg'
@@ -18,7 +15,16 @@ Page({
     vertical: false,
     autoplay: false,
     interval: 2000,
-    duration: 500
+    duration: 500,
+    serviceTypeList: [
+      { name: 'WX', value: '家电维修', checked: 'true' },
+      { name: 'QX', value: '家电清洗' },
+      { name: 'CZ', value: '家电拆装' },
+    ],
+    bookingData: {
+      imageId: ''
+    },
+    myImage: ''
   },
 
   onLoad: function() {
@@ -28,21 +34,84 @@ Page({
       })
       return
     }
+    wx.cloud.getTempFileURL({
+      fileList: ['cloud://demo-bchvt.6465-demo-bchvt-1300634251/my-image.png'],
+    }).then((res) => {
+      this.setData({
+        myImage: res.fileList[0].tempFileURL
+      })
+    })
+  },
 
-    // 获取用户信息
-    wx.getSetting({
+  imageError(e) {
+    console.log(e)
+  },
+
+  formSubmit(e) {
+    Object.assign(this.data.bookingData, e.detail.value);
+    let data = this.data.bookingData;
+    const db = wx.cloud.database();
+    db.collection('bookings').add({
+      data: data,
       success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
+        wx.showToast({
+          title: '新增记录成功',
+        })
+        console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '新增记录失败'
+        })
+        console.error('[数据库] [新增记录] 失败：', err)
+      }
+    })
+  },
+
+  // 上传图片
+  doUpload: function () {
+    let self = this;
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+
+        wx.showLoading({
+          title: '上传中',
+        })
+
+        const filePath = res.tempFilePaths[0]
+
+        // 上传图片
+        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log('[上传文件] 成功：', res)
+
+            Object.assign(self.data.bookingData, {
+              imageId: res.fileID
+            })
+          },
+          fail: e => {
+            console.error('[上传文件] 失败：', e)
+            wx.showToast({
+              icon: 'none',
+              title: '上传失败',
+            })
+          },
+          complete: () => {
+            wx.hideLoading()
+          }
+        })
+
+      },
+      fail: e => {
+        console.error(e)
       }
     })
   },
