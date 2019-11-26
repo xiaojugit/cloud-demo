62193a7d5d5e70dfe9e8c7da854fc0fd733085f4
zxj <zxj@qq.com>
2019-11-26 20:20:21 +0800

    '-'

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -16,6 +16,12 @@ Page({
     autoplay: false,
     interval: 2000,
     duration: 500,
+    serviceSteps: [
+      { title: '1、预约服务', desc: '在线预约登记', icon: '../../images/icon1.png'},
+      { title: '2、客服派单', desc: '工程师与您联系', icon: '../../images/icon2.png'},
+      { title: '3、师傅上门', desc: '检测具体故障', icon: '../../images/icon3.png'},
+      { title: '4、完成服务', desc: '开具发票和保修卡', icon: '../../images/icon4.png'},
+    ],
     serviceTypeList: [
       { name: 'WX', value: '家电维修', checked: 'true' },
       { name: 'QX', value: '家电清洗' },
